d35f9d5f7640370d6d5d7606fa5d4df377470bf3
sicilly <634068240@qq.com>
2020-01-17 19:07:59 +0800

    部署代码

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = '+ued+#)24)&hv2y&l@i5do@+#!$nc)6blf5h9=mjj32bs1f_9&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', ]
+
 
 
 # Application definition
@@ -127,3 +128,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
